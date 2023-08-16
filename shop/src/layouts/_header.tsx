54139e44d3b92b1b28a5bc8859a3b90921ce8bee
Parsa <parsa.habibi.2002@gmail.com>
2023-08-16 15:47:03 +0330

    change language done

--- shop/src/layouts/_header.tsx
+++ shop/src/layouts/_header.tsx
@@ -21,6 +21,7 @@ import Button from '@/components/ui/button';
 import LanguageSwitcher from '@/components/ui/language-switcher';
 import { useTranslation } from 'next-i18next';
 
+
 const AuthorizedMenuItems = [
   {
     label: 'text-auth-profile',
