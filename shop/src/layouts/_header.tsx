03088850ea071e81c671d7371eacffb468fbd6d5
Parsa <parsa.habibi.2002@gmail.com>
2023-08-16 15:42:34 +0330

    change language done

--- shop/src/layouts/_header.tsx
+++ shop/src/layouts/_header.tsx
@@ -147,6 +147,7 @@ export default function Header({
   return (
     <header className="app-header sticky top-0 z-30 flex h-16 w-full items-center justify-between border-b border-light-300 bg-light py-1 px-4 ltr:left-0 rtl:right-0 dark:border-dark-300 dark:bg-dark-250 sm:h-[70px] sm:px-6">
       <div className="flex items-center gap-4">
+
         {showHamburger && (
           <Hamburger
             isToggle={isCollapse}
